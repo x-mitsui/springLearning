0ef19ad1e9c53416307ca61e3e99873d39225ed7
dzq <x_mitsui@163.com>
2022-07-28 18:28:18 +0800

    注入属性：bean集合属性的处理-工具util标签对公共部分提取

---
+++ test02/src/test/java/Test05.java
@@ -0,0 +1,28 @@
+import com.dzq.bean.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+/**
+ * @Auther: x_mitsui
+ * @Date: 2022/7/28 - 07 - 28 - 13:58
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Test05 {
+    @Test
+    public void testApplicationContext4(){
+        // 默认情况下，此行已经将applicationContext.xml下所有对象都创建了
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext5.xml");
+
+        Student student1 = applicationContext.getBean("student2",Student.class);
+        System.out.println(student1.getBallList());
+        System.out.println(student1.getBookMap());
+        System.out.println(Arrays.toString(student1.getBooks()));
+        System.out.println(student1.getBookSet());
+        System.out.println(student1.getBookList());
+
+    }
+}
