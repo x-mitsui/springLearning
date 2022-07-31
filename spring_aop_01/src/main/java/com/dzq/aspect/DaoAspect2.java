d4046db0451d9f0309ceb296c9c5eaca2d6f30d8
dzq <x_mitsui@163.com>
2022-07-31 16:17:05 +0800

    Spring AOP：注解Order控制切面执行顺序

--- spring_aop_01/src/main/java/com/dzq/aspect/DaoAspect2.java
+++ spring_aop_01/src/main/java/com/dzq/aspect/DaoAspect2.java
@@ -17,41 +17,35 @@ import java.util.Arrays;
 // @Component实例化此类
 @Aspect
 @Component
-@Order(2)
-public class DaoAspect {
+@Order(1)
+public class DaoAspect2 {
     // 定义公共切点以抽取重复代码片段
     @Pointcut("execution(* com.dzq.dao.*.add(..))")// 切点表达式可以直接指向接口，屏蔽具体实现类信息，降低耦合度
     public void addPointCut(){}
 
     @Before("addPointCut()")
     public void methodBefore(JoinPoint joinPoint){
-        System.out.println("methodBefore invoked ...");
-        Object[] args = joinPoint.getArgs();
-        System.out.println(Arrays.toString(args));
-        System.out.println("被代理的对象："+joinPoint.getTarget());
-        System.out.println("代理对象："+joinPoint.getThis());
-        System.out.println("signature署名信息："+joinPoint.getSignature());
+        System.out.println("methodBefore2 invoked ...");
     }
 
     // 无论异常与否都会执行
     @After("addPointCut()")
     public void methodAfter(){
-        System.out.println("methodAfter invoked ...");
+        System.out.println("methodAfter2 invoked ...");
     }
 
     // 切点方法返回结果之后增强的功能，returning值名res对应形参res
     // 出现目标异常不执行
     @AfterReturning(value = "addPointCut()", returning = "res777")
     public void methodAfterReturn(JoinPoint joinPoint, Object res777){
-        System.out.println("methodAfterReturning invoked ...");
+        System.out.println("methodAfterReturning2 invoked ...");
     }
 
 
     // 只有目标产生异常才执行。
     @AfterThrowing(value = "addPointCut()",throwing = "ex666")
     public void methodAfterThrowing(Exception ex666){
-        System.out.println("methodAfterThrowing invoked ...");
-        System.out.println(ex666.getMessage());
+        System.out.println("methodAfterThrowing2 invoked ...");
     }
 
     // 环绕通知：切点方法之前和之后都进行功能的增强，相当于@Before+@After
@@ -59,9 +53,9 @@ public class DaoAspect {
     // 返回值必须是Object,返回值抛出去
     @Around("addPointCut()")
     public Object methodAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        System.out.println("methodAroundA invoked ...");
+        System.out.println("methodAroundA2 invoked ...");
         Object res = proceedingJoinPoint.proceed();// 控制切点方法在此处执行，addUser
-        System.out.println("methodAroundB invoked ...");
+        System.out.println("methodAroundB2 invoked ...");
         return res;
     }
 
